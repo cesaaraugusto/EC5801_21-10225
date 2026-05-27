9a13512d8719ed47b5a40ace1aada37720e44c6e
Cesar <21-10225@usb.ve>
2026-05-26 21:41:36 -0400

    Avance

--- PyYAML/main.py
+++ PyYAML/main.py
@@ -4,7 +4,17 @@ from yaml import load, dump
 
 from file_manager import file_manager
 
-class file_manager_yaml(file_manager):
+from typing import TypeVar, Generic
+
+#tipo generico
+generic_t = TypeVar("generic_t")
+
+class file_manager_yaml(file_manager, Generic[generic_t]):
     
     def __init__(self, path: Path):
         super().__init__(path)
+
+    __diccionario:generic_t
+
+    def read_file_yaml(self):
+        
