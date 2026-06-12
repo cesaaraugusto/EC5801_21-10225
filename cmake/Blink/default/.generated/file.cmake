727717a8b4dc73c674242f2b0408d71ad81e1eaa
Cesar <21-10225@usb.ve>
2026-06-12 11:49:48 -0400

    Terminado

--- cmake/Blink/default/.generated/file.cmake
+++ cmake/Blink/default/.generated/file.cmake
@@ -7,7 +7,7 @@ foreach(source_file ${Blink_default_default_XC8_FILE_TYPE_assemble})
         set_source_files_properties(${source_file} PROPERTIES INCLUDE_DIRECTORIES "$<PATH:NORMAL_PATH,$<PATH:REMOVE_FILENAME,${source_file}>>")
 endforeach()
 
-set(Blink_default_default_XC8_FILE_TYPE_assemblePreprocess)
+set(Blink_default_default_XC8_FILE_TYPE_assemblePreprocess "${CMAKE_CURRENT_SOURCE_DIR}/../../../My_MCC_Config/mcc_generated_files/clb/src/clbBitstream.S")
 set_source_files_properties(${Blink_default_default_XC8_FILE_TYPE_assemblePreprocess} PROPERTIES LANGUAGE ASM)
 
 # For assembly files, add "." to the include path for each file so that .include with a relative path works
@@ -16,15 +16,15 @@ foreach(source_file ${Blink_default_default_XC8_FILE_TYPE_assemblePreprocess})
 endforeach()
 
 set(Blink_default_default_XC8_FILE_TYPE_compile
-    "${CMAKE_CURRENT_SOURCE_DIR}/../../../My_MCC_Config/mcc/main.c"
-    "${CMAKE_CURRENT_SOURCE_DIR}/../../../My_MCC_Config/mcc/mcc_generated_files/pwm/src/pwm1.c"
-    "${CMAKE_CURRENT_SOURCE_DIR}/../../../My_MCC_Config/mcc/mcc_generated_files/system/src/clock.c"
-    "${CMAKE_CURRENT_SOURCE_DIR}/../../../My_MCC_Config/mcc/mcc_generated_files/system/src/config_bits.c"
-    "${CMAKE_CURRENT_SOURCE_DIR}/../../../My_MCC_Config/mcc/mcc_generated_files/system/src/interrupt.c"
-    "${CMAKE_CURRENT_SOURCE_DIR}/../../../My_MCC_Config/mcc/mcc_generated_files/system/src/pins.c"
-    "${CMAKE_CURRENT_SOURCE_DIR}/../../../My_MCC_Config/mcc/mcc_generated_files/system/src/system.c"
-    "${CMAKE_CURRENT_SOURCE_DIR}/../../../My_MCC_Config/mcc/mcc_generated_files/timer/src/tmr0.c"
-    "${CMAKE_CURRENT_SOURCE_DIR}/../../../My_MCC_Config/mcc/mcc_generated_files/timer/src/tmr2.c")
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../My_MCC_Config/main.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../My_MCC_Config/mcc_generated_files/clb/src/clb1.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../My_MCC_Config/mcc_generated_files/crc/src/crc.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../My_MCC_Config/mcc_generated_files/system/src/clock.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../My_MCC_Config/mcc_generated_files/system/src/config_bits.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../My_MCC_Config/mcc_generated_files/system/src/interrupt.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../My_MCC_Config/mcc_generated_files/system/src/pins.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../My_MCC_Config/mcc_generated_files/system/src/system.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../My_MCC_Config/mcc_generated_files/timer/src/tmr0.c")
 set_source_files_properties(${Blink_default_default_XC8_FILE_TYPE_compile} PROPERTIES LANGUAGE C)
 set(Blink_default_default_XC8_FILE_TYPE_link)
 set(Blink_default_image_name "default.elf")
